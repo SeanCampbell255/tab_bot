f732744f877555e7a530a0560a9017c50ebab7f7
SeanCampbell255 <campbellsean211@gmail.com>
2024-06-07 07:41:34 -0400

    Accounting interfacing
    Just making the skeleton, need to look into pandas more to see how numbers are going to be stored

--- src/services/accounting.py
+++ src/services/accounting.py
@@ -1,24 +1,46 @@
 # Big SEAN
+from decimal import Decimal # circumvent float math
 class Accounting:
     """
     Class representing mathematical operations
     Intake and adjustment of balances and tabs
     Business Rules
 
-    Add, Divide, Multiply, Percentage
+    Add, subtract, Percentage
     Calculate Balances(?)
     Calculate Amount Due (?)
     """
     pass
 
-    def add():
+    def add(self, val1, val2):
         """
+        Adds two monetary values
         """
+
         pass
-    def subtract():
+    def subtract(self, val1, val2):
         """
+        Subtracts two monetary values
         """
-        return 1 + 1 
-         
-    def cent_to_dollar():
+
         pass
+
+    def percent(self, val, percent):
+        """
+        Gets percentage of a monetary value
+        """
+
+    def toDecimal(self, val):
+        """
+        Converts float to decimal value
+        """
+
+    def toFloat(self, val):
+        """
+        Converts decimal to float value, might be unused
+        """
+
+    def sum(self, vals):
+        """
+        Sums some amount of monetary values
+        """
